047d9adeb19e9087eda7250f28c465f1c7528965
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2024-01-29 14:47:58 +0100

    Update CreateScheduleTask.ps1

--- CreateScheduleTask.ps1
+++ CreateScheduleTask.ps1
@@ -3,16 +3,21 @@
     Script permettant la création d'une tache planifiée via PowerShell
 
 .NOTES
-    Version       : 1.0.0
+    Version       : 1.0.1
     Author        : Christopher Mogis
     Creation Date : 22/01/2024
+
+    v1.0.1 : Ajout des parametres liés à la gestion de l'énergie
 #>
 
 # Action à exécuter - script Powershell RT
-$action=New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass C:\Temp\TonScript.ps1"
+$action=New-ScheduledTaskAction -Execute "powershell.exe" -Argument '-ExecutionPolicy Bypass -windowstyle hidden -file "C:\Program32\MS_Readiness\Microsoft_ReadinessToolkit_Usercontextexecution_1.2.23020_x86_EN.ps1"'
 
 # Planification de la tâche
 $trigger=New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
 
+# Parametres de la tâche
+$taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
 # Enregistrement de la tâche et "Exécuter sous"
-Register-ScheduledTask -TaskName "Execution RT" -Trigger $trigger -Action $action -Description "Readiness Toolkit Execution"
+Register-ScheduledTask -TaskName "Execution RT" -Trigger $trigger -Action $action -Description "Readiness Toolkit Execution" -Settings $taskSettings
